3d0871113160abdc8b6719d344ffe2394d312d76
Sockthing <sockthinggaming@gmail.com>
2024-07-11 05:44:36 +0200

    Added Config
    -allowGoldenDungOnHCS config only. Still needs to be implemented.

--- src/main/java/btw/community/denovo/DeNovoAddon.java
+++ src/main/java/btw/community/denovo/DeNovoAddon.java
@@ -5,12 +5,26 @@ import btw.BTWAddon;
 import btw.community.denovo.block.DNBlocks;
 import btw.community.denovo.item.DNItems;
 import btw.community.denovo.recipes.DNRecipes;
+import com.prupe.mcpatcher.mal.block.RenderBlocksUtils;
+import net.minecraft.server.MinecraftServer;
+
+import java.util.Map;
 
 public class DeNovoAddon extends BTWAddon {
     private static DeNovoAddon instance;
 
+    private Map<String, String> propertyValues;
+
+    // configuration settings
+    public static boolean allowGoldenDungOnHCS = false;
+
     private DeNovoAddon() {
         super("De Novo", "0.1.0", "DN");
+    }
+
+    @Override
+    public void preInitialize() {
+        registerConfigProperties();
     }
 
     @Override
@@ -21,6 +35,18 @@ public class DeNovoAddon extends BTWAddon {
         DNItems.initItems();
 
         DNRecipes.addRecipes();
+    }
+    private void registerConfigProperties() {
+        //Gameplay config
+        this.registerProperty("AllowGoldenDungOnHCS", "False", "Set the following to true to allow players to get a piece of golden dung on every HCS");
+    }
+
+    @Override
+    public void handleConfigProperties(Map<String, String> propertyValues) {
+        this.propertyValues = propertyValues;
+
+        allowGoldenDungOnHCS = Boolean.parseBoolean(this.propertyValues.get("AllowGoldenDungOnHCS"));
+
     }
 
     public static DeNovoAddon getInstance() {
