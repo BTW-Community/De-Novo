578bfd9e481869108cbcf1a4a9e1cb8ff55c35e2
Sockthing <sockthinggaming@gmail.com>
2024-07-15 22:18:22 +0200

    Composters or cisterns act like source blocks for farmland/planters

---
+++ src/main/java/btw/community/denovo/mixins/FarmlandBlockBaseMixin.java
@@ -0,0 +1,43 @@
+package btw.community.denovo.mixins;
+
+import btw.block.blocks.FarmlandBlockBase;
+import btw.community.denovo.block.DNBlocks;
+import btw.community.denovo.block.blocks.CisternBaseBlock;
+import btw.community.denovo.block.tileentities.CisternBaseTileEntity;
+import net.minecraft.src.Block;
+import net.minecraft.src.Material;
+import net.minecraft.src.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FarmlandBlockBase.class)
+public class FarmlandBlockBaseMixin {
+
+    @Inject(method = "hasIrrigatingBlocks", at = @At(value = "HEAD"), cancellable = true)
+    public void isConcideredIrrigationBlock(World world, int i, int j, int k, CallbackInfoReturnable<Boolean> cir) {
+        int iHorizontalRange = 4;
+
+        for ( int iTempI = i - iHorizontalRange; iTempI <= i + iHorizontalRange; iTempI++ )
+        {
+            for ( int iTempJ = j; iTempJ <= j + 1; iTempJ++ )
+            {
+                for ( int iTempK = k - iHorizontalRange; iTempK <= k + iHorizontalRange; iTempK++ )
+                {
+                    CisternBaseTileEntity cisternBase = (CisternBaseTileEntity) world.getBlockTileEntity(iTempI,iTempJ,iTempK);
+
+                    if (Block.blocksList[world.getBlockId( iTempI, iTempJ, iTempK )] instanceof CisternBaseBlock )
+                    {
+                        if (cisternBase != null && cisternBase.isFullWithWater())
+                        {
+                            cir.setReturnValue(true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+}
