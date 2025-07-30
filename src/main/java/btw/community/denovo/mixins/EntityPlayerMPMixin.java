d9f7b5d798e0fea8f8f03fc19ace6d2cd64d25d8
Sockthing <sockthinggaming@gmail.com>
2025-07-30 06:46:08 +0200

    Fix EntityPlayerMPMixin from crashing

--- src/main/java/btw/community/denovo/mixins/EntityPlayerMPMixin.java
+++ src/main/java/btw/community/denovo/mixins/EntityPlayerMPMixin.java
@@ -1,22 +1,14 @@
 package btw.community.denovo.mixins;
 
-import btw.client.fx.BTWEffectManager;
 import btw.item.BTWItems;
-import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.EntityPlayerMP;
-import net.minecraft.src.MathHelper;
-import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityPlayerMP.class)
-public abstract class EntityPlayerMPMixin extends EntityPlayer {
-    public EntityPlayerMPMixin(World par1World) {
-        super(par1World);
-    }
+public abstract class EntityPlayerMPMixin {
 
     @Inject(method = "updateGloomState",
             at = @At(
@@ -24,13 +16,15 @@ public abstract class EntityPlayerMPMixin extends EntityPlayer {
                     target = "Lnet/minecraft/src/EntityPlayerMP;attackEntityFrom(Lnet/minecraft/src/DamageSource;I)Z",
                     ordinal = 0,
                     shift = At.Shift.AFTER
-            ))
+            ),remap = false)
     public void makePlayerPoop(CallbackInfo ci){
-        this.dropItem(BTWItems.dung.itemID, 1);
+        EntityPlayerMP thisPlayer = (EntityPlayerMP)(Object)this;
 
-        worldObj.playSoundAtEntity( this, "random.explode", 0.2F, 1.25F );
+        if (thisPlayer.rand.nextInt(2) == 0) {
+            thisPlayer.dropItem(BTWItems.dung.itemID, 1);
 
-        worldObj.playSoundAtEntity(this, "mob.wolf.growl", getSoundVolume(), (rand.nextFloat() - rand.nextFloat()) * 0.2F + 1.0F);
-
+            thisPlayer.worldObj.playSoundAtEntity( thisPlayer, "random.explode", 0.2F, 1.25F );
+            thisPlayer.worldObj.playSoundAtEntity(thisPlayer, "mob.wolf.growl", 0.5F, (thisPlayer.rand.nextFloat() - thisPlayer.rand.nextFloat()) * 0.2F + 1.0F);
+        }
     }
 }
