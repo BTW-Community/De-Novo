8906b3665060f7e2f72587b1a0a3b364b1ad58c0
Sockthing <sockthinggaming@gmail.com>
2025-08-05 22:39:48 +0200

    Updated dev env to CE 3.0.0

--- install.bat
+++ install.bat
@@ -1,10 +1,28 @@
-cd "%~dp0"
-rd /s /q "./BTW_dev"
+set MAPPINGS_VERSION=1.0.3
+cd /d "%~dp0"
+rd /s /q "build_BTW\BTW_dev"
+rd /s /q "build_BTW\tmp\BTW_dev"
 call gradlew.bat --no-daemon downloadAssets
-mkdir BTW_dev
-tar.exe -xf custom_mappings/mappings_152_converted_yarn_intermediates.zip -C custom_mappings
-java -jar libs/tiny-remapper-0.8.6+local-fat.jar %~f1 "BTW_dev/%~nx1" custom_mappings/mappings/mappings.tiny intermediary named %userprofile%/.gradle/caches/fabric-loom/minecraft-1.5.2-intermediary-null.unspecified-1.0.jar
-tar.exe -xf %userprofile%/.gradle/caches/fabric-loom/1.5.2-mapped-null.unspecified-1.0/minecraft-1.5.2-mapped-null.unspecified-1.0.jar -C BTW_dev
-tar.exe -xf "BTW_dev/%~nx1" -C BTW_dev
-call gradlew.bat --no-daemon btwJar
+mkdir build_BTW\BTW_dev
+mkdir custom_mappings
+mkdir build_BTW\tmp\BTW_dev
+echo Please wait...
+tar.exe -xf mavenRepo/btw/community/mappings/%MAPPINGS_VERSION%/mappings-%MAPPINGS_VERSION%.jar -C custom_mappings
+del "%userprofile%\.gradle\caches\fabric-loom\1.6.4\minecraft-merged-intermediary.jar"
+java -jar libs/tiny-remapper-0.8.6+local-fat.jar "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged.jar" "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar" "%userprofile%/.gradle/caches/fabric-loom/1.6.4/intermediary-v2.tiny" official intermediary >nul
+tar.exe -xf "%~f1" -C build_BTW/tmp/BTW_dev
+java -jar libs/tiny-remapper-0.8.6+local-fat.jar "build_BTW/tmp/BTW_dev/BTW-CE-Intermediary.zip" "build_BTW/BTW_dev/BTW-CE-Intermediary.zip" custom_mappings/mappings/mappings.tiny intermediary named "%userprofile%/.gradle/caches/fabric-loom/1.6.4/minecraft-merged-intermediary.jar" >nul
+tar.exe -xf "%userprofile%/.gradle/caches/fabric-loom/minecraftMaven/net/minecraft/minecraft-merged/1.6.4-btw.community.mappings.1_6_4.%MAPPINGS_VERSION%-v2/minecraft-merged-1.6.4-btw.community.mappings.1_6_4.%MAPPINGS_VERSION%-v2.jar" -C build_BTW/BTW_dev
+tar.exe -xf "build_BTW/BTW_dev/BTW-CE-Intermediary.zip" -C build_BTW/BTW_dev
+del build_BTW\BTW_dev\BTW-CE-Intermediary.zip
+cd build_BTW\BTW_dev
+tar.exe -a -cf ../BTW_dev.zip *
+cd ..
+rd /s /q "BTW_dev"
+mkdir "BTW_dev"
+move BTW_dev.zip BTW_dev\BTW_dev.zip
+cd ..
+move build_BTW\tmp\BTW_dev\BTW-CE-Intermediary-javadoc.jar build_BTW\BTW_dev\BTW_dev-javadoc.jar
+rd /s /q "build_BTW\tmp\BTW_dev"
+echo Done!
 PAUSE
