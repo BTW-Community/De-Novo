e04e621b2a185eb49cbe263a8b9df4d1ddd5271a
Sockthing <sockthinggaming@gmail.com>
2025-10-30 05:28:44 +0100

    Use MaggotsSilkExtractionItem for maggotsSilkExtraction
    Replaces RustWaterBowlItem with MaggotsSilkExtractionItem for the maggotsSilkExtraction item instantiation to ensure correct item behavior and class usage.

--- src/main/java/btw/community/denovo/item/DNItems.java
+++ src/main/java/btw/community/denovo/item/DNItems.java
@@ -46,7 +46,7 @@ public class DNItems {
                 .setTextureName("denovo:maggots_cooked")
                 .setCreativeTab(CreativeTabs.tabMisc);
 
-        maggotsSilkExtraction = new RustWaterBowlItem(DeNovoAddon.instance.parseID("DNItemMaggotsSilkExtractionID") - 256, "maggots_silk_extraction");
+        maggotsSilkExtraction = new MaggotsSilkExtractionItem(DeNovoAddon.instance.parseID("DNItemMaggotsSilkExtractionID") - 256, "maggots_silk_extraction");
 
         waterBowl = new WaterBowlItem(DeNovoAddon.instance.parseID("DNItemWaterBowlID") - 256);
         rustWaterBowl = new RustWaterBowlItem(DeNovoAddon.instance.parseID("DNItemRustWaterBowlID") - 256, "rust_water_bowl");
