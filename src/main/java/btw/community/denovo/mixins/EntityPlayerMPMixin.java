6620b0302c727f7655e2e017e0be55b5133ab784
Sockthing <sockthinggaming@gmail.com>
2025-11-07 00:44:30 +0100

    Drop dung only when player takes damage in gloom
    Fixes #71

--- src/main/java/btw/community/denovo/mixins/EntityPlayerMPMixin.java
+++ src/main/java/btw/community/denovo/mixins/EntityPlayerMPMixin.java
@@ -3,6 +3,7 @@ package btw.community.denovo.mixins;
 import btw.item.BTWItems;
 import net.minecraft.src.EntityPlayerMP;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,21 +11,31 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityPlayerMP.class)
 public abstract class EntityPlayerMPMixin {
 
-    @Inject(method = "updateGloomState",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/src/EntityPlayerMP;attackEntityFrom(Lnet/minecraft/src/DamageSource;F)Z",
-                    ordinal = 0,
-                    shift = At.Shift.AFTER
-            ))
-    public void makePlayerPoop(CallbackInfo ci) {
-        EntityPlayerMP thisPlayer = (EntityPlayerMP) (Object) this;
+    @Unique
+    private float prevHealth;
 
-        if (thisPlayer.rand.nextInt(2) == 0) {
+    @Inject(method = "updateGloomState", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/src/EntityPlayerMP;attackEntityFrom(Lnet/minecraft/src/DamageSource;F)Z",
+            shift = At.Shift.BEFORE
+    ))
+    private void beforeAttack(CallbackInfo ci) {
+        this.prevHealth = ((EntityPlayerMP)(Object)this).getHealth();
+    }
+
+    @Inject(method = "updateGloomState", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/src/EntityPlayerMP;attackEntityFrom(Lnet/minecraft/src/DamageSource;F)Z",
+            shift = At.Shift.AFTER
+    ))
+    private void afterAttack(CallbackInfo ci) {
+        EntityPlayerMP thisPlayer = (EntityPlayerMP)(Object)this;
+
+        if (thisPlayer.getHealth() < this.prevHealth && thisPlayer.rand.nextInt(2) == 0) {
             thisPlayer.dropItem(BTWItems.dung.itemID, 1);
-
             thisPlayer.worldObj.playSoundAtEntity(thisPlayer, "random.explode", 0.2F, 1.25F);
-            thisPlayer.worldObj.playSoundAtEntity(thisPlayer, "mob.wolf.growl", 0.5F, (thisPlayer.rand.nextFloat() - thisPlayer.rand.nextFloat()) * 0.2F + 1.0F);
+            thisPlayer.worldObj.playSoundAtEntity(thisPlayer, "mob.wolf.growl",
+                    0.5F, (thisPlayer.rand.nextFloat() - thisPlayer.rand.nextFloat()) * 0.2F + 1.0F);
         }
     }
 }
