6f256eb2fb45dfa2f7666e20f6d8bfc71467546e
Sockthing <sockthinggaming@gmail.com>
2025-11-10 19:55:31 +0100

    Fixes Farmland Hydration from Composter
    #87

--- src/main/java/btw/community/denovo/mixins/FarmlandBlockBaseMixin.java
+++ src/main/java/btw/community/denovo/mixins/FarmlandBlockBaseMixin.java
@@ -3,7 +3,9 @@ package btw.community.denovo.mixins;
 import btw.block.blocks.FarmlandBlockBase;
 import btw.community.denovo.block.blocks.CisternBaseBlock;
 import btw.community.denovo.block.tileentities.CisternBaseTileEntity;
+import btw.community.denovo.block.tileentities.ComposterTileEntity;
 import net.minecraft.src.Block;
+import net.minecraft.src.TileEntity;
 import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,10 +22,10 @@ public class FarmlandBlockBaseMixin {
         for (int iTempI = i - iHorizontalRange; iTempI <= i + iHorizontalRange; iTempI++) {
             for (int iTempJ = j; iTempJ <= j + 1; iTempJ++) {
                 for (int iTempK = k - iHorizontalRange; iTempK <= k + iHorizontalRange; iTempK++) {
-                    CisternBaseTileEntity cisternBase = (CisternBaseTileEntity) world.getBlockTileEntity(iTempI, iTempJ, iTempK);
+                    TileEntity tileEntity  =  world.getBlockTileEntity(iTempI, iTempJ, iTempK);
 
-                    if (Block.blocksList[world.getBlockId(iTempI, iTempJ, iTempK)] instanceof CisternBaseBlock) {
-                        if (cisternBase != null && cisternBase.isFullWithWater()) {
+                    if (tileEntity instanceof ComposterTileEntity composter) {
+                        if (composter.isFullWithWater()) {
                             cir.setReturnValue(true);
                         }
                     }
